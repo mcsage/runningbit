9cc03f0a0f051a257bbe09e299c3bbd03543b7a2
Stephan Hohn <stephanhohn@gmail.com>
2020-12-25 18:43:33 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.showIcon(IconNames.Heart)
+SuperBit.MotorRunDual(
+SuperBit.enMotors.M1,
+10,
+SuperBit.enMotors.M3,
+10
+)
+basic.showIcon(IconNames.Heart)
+SuperBit.MotorRunDual(
+SuperBit.enMotors.M1,
+10,
+SuperBit.enMotors.M1,
+5
+)
